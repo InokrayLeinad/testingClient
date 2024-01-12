5db4366621a4d6a63b64f32080ef96a74587d5fc
daniekyarkoni <inokrayLeinad@gmail.com>
2024-01-12 19:22:32 +0200

    Initial commit

---
+++ src/test/java/com/example/testingClient/TestingClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
